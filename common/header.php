be2a657d8ae174fd757f204d8775b86b281a520c
KunioIkeda <157329554+KunioIkeda@users.noreply.github.com>
2024-03-20 09:12:26 +0900

    Add files via upload

--- common/header.php
+++ common/header.php
@@ -10,6 +10,7 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=BIZ+UDGothic&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Yuji+Syuku&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="common/reset.css">
         <link rel="stylesheet" href="styles/style.css">
         <title>Kunio's Portfolio</title>
