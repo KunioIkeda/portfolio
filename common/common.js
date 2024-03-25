1e37a98922187c188cb78b051ffd4cef249a8942
KunioIkeda <157329554+KunioIkeda@users.noreply.github.com>
2024-03-25 13:50:20 +0900

    Delete common/common.js

--- common/common.js
+++
@@ -1,8 +0,0 @@
-// 'use strict';
-
-// fetch("common/header.html")
-//         .then((response) => response.text())
-//         .then((data) => document.querySelector("#header").innerHTML = data);
-// fetch("common/footer.html")
-//         .then((response) => response.text())
-//         .then((data) => document.querySelector("#footer").innerHTML = data);
